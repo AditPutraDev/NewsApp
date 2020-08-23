05763345e43873991a6822684a0ecd0079d498c3
Aditya1406incom <aditya_hermansyahputra@yahoo.com>
2020-08-23 17:58:07 +0700

    update url

--- app/src/main/java/id/mentoring/news/network/NewsService.kt
+++ app/src/main/java/id/mentoring/news/network/NewsService.kt
@@ -5,6 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface NewsService {
-    @GET("v2/everything?q=bitcoin&from=2020-07-22&sortBy=publishedAt&apiKey=d32dd7d06e3b40b8ab47fb94dfbe8ac4")
+    @GET("v2/everything?q=bitcoin&from=2020-07-23&sortBy=publishedAt&apiKey=d32dd7d06e3b40b8ab47fb94dfbe8ac4")
     fun getNews(): Call<ResponseServer>
 }
